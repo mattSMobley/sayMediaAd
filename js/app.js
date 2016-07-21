92789c2ef31d64a6c90b531d1a94a51976ea3d80
Matt Mobley <mattsmobley@gmail.com>
2016-07-21 13:18:48 -0600

    commented and cleaned up code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+
+//Opens Video Overlay
 var overlay = document.getElementsByClassName("videoOverlay");
 document.getElementById("viewVideoButton").onclick = function() {
     overlay[0].style.display = "inline";
 }
+
+//Closes Video Overlay
 document.getElementsByClassName("exitVideoButton")[0].onclick = function() {
     overlay[0].style.display = "none";
 }
